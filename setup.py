37a35b07c0deb2c051204407abfa21a0e5ca349d
YanzuWuu <43078590+YanzuWuu@users.noreply.github.com>
2019-02-25 17:14:52 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ex2',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Yuncheng Zhu',
+    author_email='yczhu1@bu.edu',
+    description='use nsync and ffmpeg to transfer video inot two version'
+)
