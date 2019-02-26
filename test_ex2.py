ccc1cd9291809bafb4010cc19f168c2215e49def
YanzuWuu <43078590+YanzuWuu@users.noreply.github.com>
2019-02-26 09:57:42 -0500

    Update test_ex2.py

--- test_ex2.py
+++ test_ex2.py
@@ -1,8 +1,8 @@
 import subprocess
-
 from pytest import approx
 import json
 from pathlib import Path
+import Exercise_2 as run
 def ffprobe_sync(filein: Path) -> dict:
     """ get media metadata """
     meta = subprocess.check_output(['ffprobe', '-v', 'warning','-print_format', 'json','-show_streams','-show_format',str(filein)], universal_newlines = True)
@@ -34,3 +34,7 @@ def test_duration():
     duration_720_22 = float(meta_720['streams'][0]['duration'])
     assert orig_duration == approx(duration_480) == approx(duration_720)
     assert orig_duration1 == approx(duration_480_22) == approx(duration_720_22)
+
+def main():
+    run.convert_video()
+    test_duration()
