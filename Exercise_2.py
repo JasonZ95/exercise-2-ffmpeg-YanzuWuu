44370812328eac926133ff45c9554cdc687ca57a
YanzuWuu <43078590+YanzuWuu@users.noreply.github.com>
2019-02-26 00:23:03 -0500

    Change the save path from my computer to the current one

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,23 +6,23 @@ import time
 import pytest
 
 def convert_video(path):
-    files= os.listdir(path)
+    files= os.listdir(path+'/in/')
     q = queue.Queue()
     i = 1
     for file in files:
-        # print(file)
+        print(file)
         q.put(file)
     while not q.empty():
         video = q.get()
         async def transfer_720p():
             try:
-                subprocess.call('ffmpeg -i ' +path + '/' + video + ' -b 2M -r 30 -s 1280x720 -c:a copy'+' /Users/y/PycharmProjects/ec500/out/30fps+2Mbps+720p_'+str(i)+'.mp4', shell=True)
+                subprocess.call('ffmpeg -i ' +path + '/in/' + video + ' -b 2M -r 30 -s 1280x720 -c:a copy '+path+'/out/720_'+video+str(i)+'.mp4', shell=True)
                 return '720p videos all transeferred'
             except:
                 return 'transfer failed'
         async def transfer_480p():
             try:
-                subprocess.call('ffmpeg -i '+ path + '/' + video +' -b 1M -r 30 -s 720x480 -c:a copy'+' /Users/y/PycharmProjects/ec500/out/30fps+1Mbps+480p_'+str(i)+'.mp4', shell=True)
+                subprocess.call('ffmpeg -i '+ path + '/in/' + video +' -b 1M -r 30 -s 720x480 -c:a copy '+path+'/out/480_'+video+str(i)+'.mp4', shell=True)
                 return '480p videos all transeferred'
             except:
                 return 'transfer failed'
@@ -39,20 +39,26 @@ def convert_video(path):
     print(str(i-1) +' videos have been transferred into 720p and 480p type')
 
 # def test_convert_video(path):
-#     files= os.listdir(path)
+#     files= os.listdir(path+'/in/')
 #     q = queue.Queue()
 #     i = 1
 #     for file in files:
-#         # print(file)
+#         print(file)
 #         q.put(file)
 #     while not q.empty():
 #         video = q.get()
 #         async def transfer_720p():
-#             subprocess.call('ffmpeg -i ' +path + '/' + video + ' -b 2M -r 30 -s 1280x720 -c:a copy'+' /Users/y/PycharmProjects/ec500/out/30fps+2Mbps+720p_'+str(i)+'.mp4', shell=True)
-#             return '720p videos all transeferred'
+#             try:
+#                 subprocess.call('ffmpeg -i ' +path + '/in/' + video + ' -b 2M -r 30 -s 1280x720 -c:a copy '+path+'/out/720_'+video+str(i)+'.mp4', shell=True)
+#                 return '720p videos all transeferred'
+#             except:
+#                 return 'transfer failed'
 #         async def transfer_480p():
-#             subprocess.call('ffmpeg -i '+ path + '/' + video +' -b 1M -r 30 -s 720x480 -c:a copy'+' /Users/y/PycharmProjects/ec500/out/30fps+1Mbps+480p_'+str(i)+'.mp4', shell=True)
-#             return '480p videos all transeferred'
+#             try:
+#                 subprocess.call('ffmpeg -i '+ path + '/in/' + video +' -b 1M -r 30 -s 720x480 -c:a copy '+path+'/out/480_'+video+str(i)+'.mp4', shell=True)
+#                 return '480p videos all transeferred'
+#             except:
+#                 return 'transfer failed'
 #
 #         tasks = [asyncio.ensure_future(transfer_720p()),asyncio.ensure_future(transfer_480p()),]
 #         loop = asyncio.get_event_loop()
@@ -68,7 +74,7 @@ def convert_video(path):
 def main():
     start = time.clock()
     path = os.getcwd()
-    convert_video(path+'/in/')
+    convert_video(path)
     elapsed = time.clock()-start
     print("Time used:",elapsed)
 
